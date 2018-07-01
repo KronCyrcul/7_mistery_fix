f746126131bd6fb915873848d1c073272f047a53
KronCyrcul <ibloko1504@gmail.com>
2018-07-01 13:08:02 +0300

    Add condition for complex discriminant

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -2,6 +2,8 @@ from math import sqrt
 
 
 def get_roots(a, b, c):
+    if 4*a*c > b**2:
+        return None, None
     discriminant = b ** 2 - 4 * a * c
     root1 = (-b - sqrt(discriminant)) / (2 * a)
     root2 = (-b + sqrt(discriminant)) / (2 * a)
